8ed4224b000a36abb9f763377dd0e9b10ce90a93
Mario Moreira <78828831+mario-moreira-ads@users.noreply.github.com>
2021-10-20 18:23:09 -0300

    Add files via upload

---
+++ sistemaPassagemOnibus/Trabalho/trabalho.git/MicroOnibus.java
@@ -0,0 +1,11 @@
+
+public class MicroOnibus extends Veiculo
+{
+   public MicroOnibus(String NumeroPassagem,String tipo,int Capacidade,int EhIdoso){
+        this.NumeroPassagem = NumeroPassagem;
+        this.tipo= "Micro-Onibus";
+        this.Capacidade = 20;
+        this.EhIdoso = 2;
+    
+    } 
+}
